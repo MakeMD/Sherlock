9ed8e2c83a23d908a399234cc83c16b9de5c34fa
MakeMD <admin.rio@gmail.com>
2013-11-04 18:56:43 +0200

    add widget

--- gen/com/chemart/sherlock/R.java
+++ gen/com/chemart/sherlock/R.java
@@ -17,8 +17,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int algiz=0x7f020000;
@@ -32,119 +32,122 @@ public final class R {
         public static final int eihwaz=0x7f020008;
         public static final int fehu=0x7f020009;
         public static final int gebo=0x7f02000a;
-        public static final int hagalaz=0x7f02000b;
-        public static final int ic_launcher=0x7f02000c;
-        public static final int inguz=0x7f02000d;
-        public static final int isa=0x7f02000e;
-        public static final int jera=0x7f02000f;
-        public static final int kano=0x7f020010;
-        public static final int laguz=0x7f020011;
-        public static final int mannaz=0x7f020012;
-        public static final int nauthiz=0x7f020013;
-        public static final int original=0x7f020014;
-        public static final int orn_1=0x7f020015;
-        public static final int othilia=0x7f020016;
-        public static final int perth=0x7f020017;
-        public static final int raido=0x7f020018;
-        public static final int sowelu=0x7f020019;
-        public static final int stone=0x7f02001a;
-        public static final int teivaz=0x7f02001b;
-        public static final int text_color=0x7f020020;
-        public static final int thurisaz=0x7f02001c;
-        public static final int uruz=0x7f02001d;
-        public static final int weird=0x7f02001e;
-        public static final int wunjo=0x7f02001f;
+        public static final int glow=0x7f02000b;
+        public static final int hagalaz=0x7f02000c;
+        public static final int ic_launcher=0x7f02000d;
+        public static final int inguz=0x7f02000e;
+        public static final int isa=0x7f02000f;
+        public static final int jera=0x7f020010;
+        public static final int kano=0x7f020011;
+        public static final int laguz=0x7f020012;
+        public static final int mannaz=0x7f020013;
+        public static final int nauthiz=0x7f020014;
+        public static final int original=0x7f020015;
+        public static final int orn_1=0x7f020016;
+        public static final int othilia=0x7f020017;
+        public static final int perth=0x7f020018;
+        public static final int raido=0x7f020019;
+        public static final int sowelu=0x7f02001a;
+        public static final int stone=0x7f02001b;
+        public static final int teivaz=0x7f02001c;
+        public static final int text_color=0x7f020022;
+        public static final int thurisaz=0x7f02001d;
+        public static final int uruz=0x7f02001e;
+        public static final int weird=0x7f02001f;
+        public static final int widge=0x7f020020;
+        public static final int wunjo=0x7f020021;
     }
     public static final class id {
-        public static final int ImageButton01=0x7f08000f;
-        public static final int ImageButton02=0x7f080004;
-        public static final int ImageButton03=0x7f080012;
-        public static final int ImageButton04=0x7f080010;
-        public static final int ImageButton05=0x7f080013;
-        public static final int ImageButton06=0x7f08000b;
-        public static final int ImageButton07=0x7f080018;
-        public static final int ImageButton08=0x7f080006;
-        public static final int ImageButton09=0x7f080003;
-        public static final int ImageButton10=0x7f080007;
-        public static final int ImageButton11=0x7f080002;
-        public static final int ImageButton12=0x7f080005;
-        public static final int ImageButton13=0x7f08000c;
-        public static final int ImageButton14=0x7f08000a;
-        public static final int ImageButton15=0x7f080016;
-        public static final int ImageButton16=0x7f080001;
-        public static final int ImageButton17=0x7f080014;
-        public static final int ImageButton18=0x7f080015;
-        public static final int ImageButton19=0x7f080019;
-        public static final int ImageButton20=0x7f080011;
-        public static final int ImageButton21=0x7f080008;
-        public static final int ImageButton22=0x7f08000d;
-        public static final int ImageButton23=0x7f080009;
-        public static final int ImageButton24=0x7f080017;
-        public static final int RelativeLayout1=0x7f080000;
-        public static final int action_settings=0x7f08001e;
-        public static final int imageButton1=0x7f08000e;
-        public static final int imageView1=0x7f08001a;
-        public static final int imageView2=0x7f08001c;
-        public static final int textView1=0x7f08001b;
-        public static final int textView2=0x7f08001d;
+        public static final int ImageButton01=0x7f09000f;
+        public static final int ImageButton02=0x7f090004;
+        public static final int ImageButton03=0x7f090012;
+        public static final int ImageButton04=0x7f090010;
+        public static final int ImageButton05=0x7f090013;
+        public static final int ImageButton06=0x7f09000b;
+        public static final int ImageButton07=0x7f090018;
+        public static final int ImageButton08=0x7f090006;
+        public static final int ImageButton09=0x7f090003;
+        public static final int ImageButton10=0x7f090007;
+        public static final int ImageButton11=0x7f090002;
+        public static final int ImageButton12=0x7f090005;
+        public static final int ImageButton13=0x7f09000c;
+        public static final int ImageButton14=0x7f09000a;
+        public static final int ImageButton15=0x7f090016;
+        public static final int ImageButton16=0x7f090001;
+        public static final int ImageButton17=0x7f090014;
+        public static final int ImageButton18=0x7f090015;
+        public static final int ImageButton19=0x7f090019;
+        public static final int ImageButton20=0x7f090011;
+        public static final int ImageButton21=0x7f090008;
+        public static final int ImageButton22=0x7f09000d;
+        public static final int ImageButton23=0x7f090009;
+        public static final int ImageButton24=0x7f090017;
+        public static final int RelativeLayout1=0x7f090000;
+        public static final int action_settings=0x7f09001e;
+        public static final int imageButton1=0x7f09000e;
+        public static final int imageView1=0x7f09001a;
+        public static final int imageView2=0x7f09001c;
+        public static final int textView1=0x7f09001b;
+        public static final int textView2=0x7f09001d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_second=0x7f030001;
+        public static final int widget_activity_main=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int Description=0x7f05002d;
-        public static final int ImageView=0x7f05002c;
-        public static final int Runa_description=0x7f050030;
-        public static final int Runa_name=0x7f05002f;
-        public static final int Runas=0x7f05002e;
-        public static final int action_settings=0x7f050001;
-        public static final int activity_second=0x7f050002;
-        public static final int algiz_down=0x7f05000b;
-        public static final int algiz_up=0x7f05000a;
-        public static final int ansuz_down=0x7f05001b;
-        public static final int ansuz_up=0x7f05001a;
-        public static final int app_name=0x7f050000;
-        public static final int berkana_down=0x7f05001f;
-        public static final int berkana_up=0x7f05001e;
-        public static final int choise=0x7f05002b;
-        public static final int dagaz=0x7f050028;
-        public static final int ehwaz_down=0x7f050005;
-        public static final int ehwaz_up=0x7f050004;
-        public static final int eihwaz=0x7f05002a;
-        public static final int fehu_down=0x7f050019;
-        public static final int fehu_up=0x7f050018;
-        public static final int gebo=0x7f050008;
-        public static final int hagalaz=0x7f050009;
-        public static final int img_button=0x7f050003;
-        public static final int inguz=0x7f050025;
-        public static final int isa=0x7f050014;
-        public static final int jera=0x7f050015;
-        public static final int kano_down=0x7f050013;
-        public static final int kano_up=0x7f050012;
-        public static final int laguz_down=0x7f050024;
-        public static final int laguz_up=0x7f050023;
-        public static final int mannaz_down=0x7f05001d;
-        public static final int mannaz_up=0x7f05001c;
-        public static final int nauthiz_down=0x7f05000f;
-        public static final int nauthiz_up=0x7f05000e;
-        public static final int othilia_down=0x7f050027;
-        public static final int othilia_up=0x7f050026;
-        public static final int perth_down=0x7f050017;
-        public static final int perth_up=0x7f050016;
-        public static final int raido_down=0x7f05000d;
-        public static final int raido_up=0x7f05000c;
-        public static final int sowelu=0x7f050022;
-        public static final int teivaz_down=0x7f050021;
-        public static final int teivaz_up=0x7f050020;
-        public static final int uruz_down=0x7f050007;
-        public static final int uruz_up=0x7f050006;
-        public static final int weird=0x7f050029;
-        public static final int wunjo_down=0x7f050011;
-        public static final int wunjo_up=0x7f050010;
+        public static final int Description=0x7f06002d;
+        public static final int ImageView=0x7f06002c;
+        public static final int Runa_description=0x7f060030;
+        public static final int Runa_name=0x7f06002f;
+        public static final int Runas=0x7f06002e;
+        public static final int action_settings=0x7f060001;
+        public static final int activity_second=0x7f060002;
+        public static final int algiz_down=0x7f06000b;
+        public static final int algiz_up=0x7f06000a;
+        public static final int ansuz_down=0x7f06001b;
+        public static final int ansuz_up=0x7f06001a;
+        public static final int app_name=0x7f060000;
+        public static final int berkana_down=0x7f06001f;
+        public static final int berkana_up=0x7f06001e;
+        public static final int choise=0x7f06002b;
+        public static final int dagaz=0x7f060028;
+        public static final int ehwaz_down=0x7f060005;
+        public static final int ehwaz_up=0x7f060004;
+        public static final int eihwaz=0x7f06002a;
+        public static final int fehu_down=0x7f060019;
+        public static final int fehu_up=0x7f060018;
+        public static final int gebo=0x7f060008;
+        public static final int hagalaz=0x7f060009;
+        public static final int img_button=0x7f060003;
+        public static final int inguz=0x7f060025;
+        public static final int isa=0x7f060014;
+        public static final int jera=0x7f060015;
+        public static final int kano_down=0x7f060013;
+        public static final int kano_up=0x7f060012;
+        public static final int laguz_down=0x7f060024;
+        public static final int laguz_up=0x7f060023;
+        public static final int mannaz_down=0x7f06001d;
+        public static final int mannaz_up=0x7f06001c;
+        public static final int nauthiz_down=0x7f06000f;
+        public static final int nauthiz_up=0x7f06000e;
+        public static final int othilia_down=0x7f060027;
+        public static final int othilia_up=0x7f060026;
+        public static final int perth_down=0x7f060017;
+        public static final int perth_up=0x7f060016;
+        public static final int raido_down=0x7f06000d;
+        public static final int raido_up=0x7f06000c;
+        public static final int sowelu=0x7f060022;
+        public static final int teivaz_down=0x7f060021;
+        public static final int teivaz_up=0x7f060020;
+        public static final int uruz_down=0x7f060007;
+        public static final int uruz_up=0x7f060006;
+        public static final int weird=0x7f060029;
+        public static final int wunjo_down=0x7f060011;
+        public static final int wunjo_up=0x7f060010;
     }
     public static final class style {
         /** 
@@ -157,10 +160,13 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int widget_metadata=0x7f040000;
     }
 }
